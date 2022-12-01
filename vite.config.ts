769b2f41d999c62852f1758682aea8137b525f4f
Misha Malyshev <misha@playdex.io>
2022-12-02 01:01:49 +0800

    :bug: remove vite react plugin, add react-dom to external packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
-import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
         dts({
             insertTypesEntry: true,
         }),
@@ -25,6 +23,7 @@ export default defineConfig({
             output: {
                 globals: {
                     react: 'React',
+                    'react-dom': 'ReactDOM',
                 },
             },
         },
